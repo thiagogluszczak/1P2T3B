373e38f4a0d548038c355546516486114d74c6e6
thiagogluszczak <101815123+thiagogluszczak@users.noreply.github.com>
2024-07-25 08:30:52 -0300

    Reminder
    Arquivos base para o projeto 1P2T3B.

---
+++ 1P2T3B/main.js
@@ -0,0 +1,7 @@
+const button__div = document.querySelector('.buttons');
+const buttons__array = []
+
+for (let index = 0; index < buttons__array.length; index++) {
+    let element = buttons__array[index];
+    
+}
