b13ee8607ff805e4556f86ea3f263a535359af0d
thiagogluszczak <101815123+thiagogluszczak@users.noreply.github.com>
2024-08-01 09:07:25 -0300

    Reminder
    Atualização feita no dia 01/08/2024, às 09:07.

--- 1P2T3B/main.js
+++ 1P2T3B/main.js
@@ -1,7 +1,8 @@
-const button__div = document.querySelector('.buttons');
-const buttons__array = []
+const reminders = document.querySelector('.reminders');
+const reminds__array = [["Passar na 1ª fase da UFPR",Date('20/10/2024')],["Passar na 2ª fase da UFPR",Date('20/10/2024')],["Abrir uma empresa",Date('20/10/2024')],["Ler 10 livros",Date('20/10/2024')],["Escrever um artigo científico",Date('20/10/2024')]]
+var today = Date();
 
-for (let index = 0; index < buttons__array.length; index++) {
-    let element = buttons__array[index];
-    
+for (let index = 0; index < reminds__array.length; index++) {
+    let element = reminds__array[index];
+    reminders.innerHTML = "";
 }
